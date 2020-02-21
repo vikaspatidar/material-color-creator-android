bde3afd783833098754bd8fea3c64dcf6ab11e41
Vikas Patidar <vikas.patidar@outlook.com>
2020-02-21 13:18:13 +0530

    AndroidX migration

--- Sample/src/main/java/com/shopgun/android/materialcolorcreator/sample/MainActivity.java
+++ Sample/src/main/java/com/shopgun/android/materialcolorcreator/sample/MainActivity.java
@@ -3,7 +3,7 @@ package com.shopgun.android.materialcolorcreator.sample;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
